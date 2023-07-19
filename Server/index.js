c0aae28fe876f01c2e3f88a4ffb8679e7e118702
Nihal Kishore <nihal301298@gmail.com>
2023-07-19 13:53:54 +0530

    Dall - E clone Done\

---
+++ Server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import postRoutes from "./routes/postRoutes.js"
+import dalleRoutes from "./routes/dalleRoutes.js"
+
+import connectDB from "./mongoDB/Connect.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "500mb" }));
+
+app.use("/api/v1/post",postRoutes)
+app.use("/api/v1/dalle",dalleRoutes)
+
+app.get("/", async (req, res) => {
+  res.send("Hello from Dall-E");
+});
+
+const startServer = async () =>{
+    try{
+        connectDB(process.env.MONGODB_URL);
+        app.listen(8080 ,()=> console.log("Server has started on Port http://localhost:8080"))
+
+
+    }catch(error){
+
+    }
+    
+}
+
+startServer();
